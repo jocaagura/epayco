391c27ab5e4872396a8cad9063968db8b819d1d8
jocaagura <ventas@jocaagura.com>
2020-09-17 16:32:53 -0500

    Inicio del proyecto

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+## [0.0.1] - Septiembre 2020
+* Inicio de proyecto para metodos de pago con epayco en colombia
---
+++ lib/epayco.dart
@@ -0,0 +1,7 @@
+library epayco;
+
+/// A Calculator.
+class Epayco {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
